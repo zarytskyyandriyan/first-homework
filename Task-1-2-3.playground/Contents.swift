531be123f41c1ce269b5023c43ec38ce14b0ca38
Andriyan <andrianzarickij@gamil.com>
2023-06-25 14:20:28 +0200

    put in func

--- Task-1-2-3.playground/Contents.swift
+++ Task-1-2-3.playground/Contents.swift
@@ -1,22 +1,34 @@
 import Foundation
 
-var randomNumbers = [43, -11, 2, 9, -20, -1, 5, 13, -32, -67]
-
-print(randomNumbers)
-
-
-for index in 0..<randomNumbers.count {
-    if randomNumbers[index] < 0 {
-        randomNumbers[index] = 0
+func numbers() {
+    var randomNumbers: Array<Int> = []
+    for _ in 1...5 {
+        randomNumbers.append(Int.random(in: -100...0))
     }
-}
-print(randomNumbers)
 
-print("Найменше число в масиві це: ", randomNumbers.min())
-print("Найбільше число в масиві це: ", randomNumbers.max())
+    for _ in 1...5 {
+        randomNumbers.append(Int.random(in: 0...100))
+    }
+    print(randomNumbers)
 
-var sum = 0
-for i in randomNumbers {
-    sum += i
+
+    for index in 0..<randomNumbers.count {
+        if randomNumbers[index] < 0 {
+            randomNumbers[index] = 0
+        }
+    }
+    print(randomNumbers)
+
+    print("Найменше число в масиві це: ", randomNumbers.min())
+    print("Найбільше число в масиві це: ", randomNumbers.max())
+
+    var sum = 0
+    for i in randomNumbers {
+        sum += i
+    }
+    print("сумма всіх чисел", sum)
+
 }
-print("сумма всіх чисел", sum)
+
+numbers()
+
